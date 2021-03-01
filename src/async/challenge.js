4b5fc3794160fdbe7e5fe40c3f77dd1df6554243
Jose Padron <josepadron.go@gmail.com>
2021-02-28 22:45:08 -0500

    improving async challenge

--- src/async/challenge.js
+++ src/async/challenge.js
@@ -7,13 +7,18 @@ const anotherFuction = async (url_api) => {
     const data = await fetchData(url_api);
     const character = await fetchData(`${API}${data.results[0].id}`);
     const origin = await fetchData(character.origin.url);
+    const residentsPromises = origin.residents.map((data) => fetchData(data));
+    const residents = await Promise.all(residentsPromises).then((resolve) => resolve);
+
+    let livingWith = "";
+    for (names of residents) livingWith += `${names.name}, `;
 
     console.log(data.info.count);
     console.log(character.name);
     console.log(origin.dimension);
-  } catch (error) {
-    console.error(error);
-  }
+    console.log(`Live With: ${livingWith.slice(0, livingWith.length - 2)}.`);
+
+  } catch {console.error}
 };
 
 anotherFuction(API);
--- src/async/index.js
+++ src/async/index.js
@@ -1,8 +1,6 @@
 doSomethingAsync = () => {
   return new Promise((resolve, reject)  =>  {
-    (true)
-      ? setTimeout(() => resolve('do something async'), 2000)
-      : reject(new Error('test error'))
+    (true) ? setTimeout(() => resolve('do something async'), 2000) : reject(new Error('test error'))
   })
 }
 
@@ -11,19 +9,13 @@ const doSomething = async () => {
   console.log(something)
 }
 
-console.log('before 1')
-doSomething()
-console.log('before 2')
-
 const anotherFuction = async () => {
   try {
     const something = await doSomethingAsync()
     console.log(something)
-  } catch (error) {
-    console.error(error)
-  }
+
+  } catch {console.error}
 }
 
-console.log('before 3')
+doSomething()
 anotherFuction()
-console.log('before 4')
